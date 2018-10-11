a109f8bb214764b2baa090c5d497f770e370f28d
Firestar493 <bradonzhang@gmail.com>
2018-10-11 18:10:52 -0400

    Total Members -> Total Member Count
    Improved grammar

--- modules/info.py
+++ modules/info.py
@@ -196,7 +196,7 @@ class Info:
         e = discord.Embed(title="Hello from lmao-bot! 👋", color=0xFF2500, description=desc)
         e.set_thumbnail(url=self.bot.user.avatar_url)
         e.add_field(name="Server Count", value=str(len(self.bot.guilds)))
-        e.add_field(name="Total Members", value=usage.count_total_members(self.bot))
+        e.add_field(name="Total Member Count", value=usage.count_total_members(self.bot))
         e.add_field(name="Invite me to your server", value="[You won't regret it 👀](https://discordapp.com/oauth2/authorize?client_id=459432854821142529&scope=bot&permissions=336063575)")
         e.add_field(name="Join the support server", value="[Send help pls](https://discord.gg/JQgB7p7)")
         e.add_field(name="Vote for me on Discord Bot List", value="[The power is in your hands](https://discordbots.org/bot/459432854821142529/vote)")
