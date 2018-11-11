9042e39a3c7b38243786c2d28620a071631bd349
Firestar493 <bradonzhang@gmail.com>
2018-11-10 20:19:13 -0500

    CUSTOM FILTERS (plus some cleaner code for secret.py)
    Admins and people with Manage Messages now have the ability to manage custom filters.
    
    lmao filter, lmao filter add, lmao filter edit, lmao filter flags, and lmao filter remove added as commands.
    
    secret.py now has a local check to see if the person using it is a lmao developer or not.

--- modules/secret.py
+++ modules/secret.py
@@ -13,55 +13,56 @@ class Secret:
     def __init__(self, bot):
         self.bot = bot
 
+    async def __local_check(self, ctx):
+        is_dev = perms.is_lmao_developer(ctx.message)
+        if not is_dev:
+            await self.bot.get_command("replaceass").invoke(ctx)
+        return is_dev
+
     @commands.command(name="announce", hidden=True)
     async def cmd_announce(self, ctx, *, arg=""):
-        if perms.is_lmao_developer(ctx.message):
-            for guild in self.bot.guilds:
-                for channel in guild.text_channels:
-                    try:
-                        if channel.permissions_for(guild.me).send_messages:
-                            with io.open("management/announcement_finished.txt", "a") as f:
-                                f.write(f"{guild.name} ({guild.id})")
-                            await channel.send(arg)
-                            break
-                    except AttributeError:
-                        pass
-                    except discord.errors.Forbidden:
-                        pass
-                await asyncio.sleep(10)
+        for guild in self.bot.guilds:
+            for channel in guild.text_channels:
+                try:
+                    if channel.permissions_for(guild.me).send_messages:
+                        with io.open("management/announcement_finished.txt", "a") as f:
+                            f.write(f"{guild.name} ({guild.id})")
+                        await channel.send(arg)
+                        break
+                except AttributeError:
+                    pass
+                except discord.errors.Forbidden:
+                    pass
+            await asyncio.sleep(10)
         usage.update(ctx)
         return ctx.command.name
 
     @commands.command(name="changemaintenance", hidden=True)
     async def cmd_change_maintenance(self, ctx, *, arg=""):
-        if perms.is_lmao_developer(ctx.message):
-            lbvars.set_maintenance_time(arg)
-            await self.bot.change_presence(activity=discord.Game(name=f"lmao help | Maint.: {lbvars.maintenance_time} | Firestar493#6963"))
-            lbvars.custom_game = True
+        lbvars.set_maintenance_time(arg)
+        await self.bot.change_presence(activity=discord.Game(name=f"lmao help | Maint.: {lbvars.maintenance_time} | Firestar493#6963"))
+        lbvars.custom_game = True
         usage.update(ctx)
         return ctx.command.name
 
     @commands.command(name="changegame", hidden=True)
     async def cmd_change_game(self, ctx, *, arg=""):
-        if perms.is_lmao_developer(ctx.message):
-            await self.bot.change_presence(activity=discord.Game(name=arg))
-            lbvars.custom_game = True
+        await self.bot.change_presence(activity=discord.Game(name=arg))
+        lbvars.custom_game = True
         usage.update(ctx)
         return ctx.command.name
 
     @commands.command(name="displaymaintenance", hidden=True)
     async def cmd_display_maintenance(self, ctx):
-        if perms.is_lmao_developer(ctx.message):
-            await self.bot.change_presence(activity=discord.Game(name=f"lmao help | Maint.: {lbvars.maintenance_time} | Firestar493#6963"))
-            lbvars.custom_game = True
+        await self.bot.change_presence(activity=discord.Game(name=f"lmao help | Maint.: {lbvars.maintenance_time} | Firestar493#6963"))
+        lbvars.custom_game = True
         usage.update(ctx)
         return ctx.command.name
 
     @commands.command(name="displayguildcount", hidden=True)
     async def cmd_display_guild_count(self, ctx):
-        if perms.is_lmao_developer(ctx.message):
-            await self.bot.change_presence(activity=discord.Game(name=f"lmao help | in {len(self.bot.guilds)} guilds | Firestar493#6963"))
-            lbvars.custom_game = False
+        await self.bot.change_presence(activity=discord.Game(name=f"lmao help | in {len(self.bot.guilds)} guilds | Firestar493#6963"))
+        lbvars.custom_game = False
         usage.update(ctx)
         return ctx.command.name
 
