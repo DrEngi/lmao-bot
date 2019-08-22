0879ad2769001dc21d3f922c31813007f8f4288b
Dr. Engineer <6692751+DrEngi@users.noreply.github.com>
2019-08-21 21:18:43 -0400

    8ball works now: )

--- lmao-bot/Modules/ProbabilityModule.cs
+++ lmao-bot/Modules/ProbabilityModule.cs
@@ -125,6 +125,7 @@ namespace lmao_bot.Modules
 
         [Command("pick")]
         [Alias("choose", "select")]
+        [Summary("Pick from a list of options. Seperate options with ,")]
         public async Task Pick([Remainder] string optionsStr)
         {
             string[] options = optionsStr.Split(",");
@@ -149,6 +150,51 @@ namespace lmao_bot.Modules
             int x = rdm.Next(0, pickMessage.Count);
             int y = rdm.Next(0, options.Length);
             await ReplyAsync(Context.User.Mention + " " + pickMessage[x] + options[y].Trim() + afterMessage[x]);
+        }
+
+        [Command("8ball")]
+        [Alias("eightball", "8-ball")]
+        [Summary("Ask the magic 8ball something")]
+        public async Task EightBall([Remainder] string query)
+        {
+            List<string> responses = new List<string>()
+            {
+                "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."
+            };
+
+            Random rnd = new Random();
+            int rand = rnd.Next(0, 19);
+            string emoji = ":negative_squared_cross_mark";
+
+            if (rand < 10)
+            {
+                emoji = ":white_check_mark:";
+            }
+            else if (rand < 15)
+            {
+                emoji = ":question:";
+            }
+            await ReplyAsync(emoji + " " + Context.User.Mention + " " + responses[rand]);
+
         }
     }
 }
